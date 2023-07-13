76fa55f3991aa876643dba2faeec9fb14d427d17
Jens <jens@totemic.com>
2023-07-13 14:34:36 -0700

    Upgraded DSPFilters

--- DSPFilters/conanfile.py
+++ DSPFilters/conanfile.py
@@ -1,31 +1,45 @@
-from conans import ConanFile, CMake, tools
+from conan import ConanFile
+from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
+from conan.tools.files import get
 
+required_conan_version = ">=1.53.0"
 
 class DSPFiltersRecipe(ConanFile):
     name = "DSPFilters"
+    version = "cci.20170309"
     homepage = "https://github.com/vinniefalco/DSPFilters"
     description = "A Collection of Useful C++ Classes for Digital Signal Processing"
     url = "https://github.com/vinniefalco/DSPFilters.git"
     license = "MIT"
     settings = "os", "arch", "compiler", "build_type"
 
-    exports = ["CMakeLists.txt"]
+    exports_sources = ["CMakeLists.txt"]
+
+    def layout(self):
+        cmake_layout(self)
 
     def source(self):
-        tools.get(**self.conan_data["sources"][self.version], strip_root=True)
+        get(self, **self.conan_data["sources"][self.version], strip_root=True)
 
-    def _configure_cmake(self):
-        cmake = CMake(self)
-        cmake.configure()
-        return cmake
+    def generate(self):
+        tc = CMakeToolchain(self)
+        tc.generate()
+        deps = CMakeDeps(self)
+        deps.generate()
 
     def build(self):
-        cmake = self._configure_cmake()
+        cmake = CMake(self)
+        cmake.configure()
         cmake.build()
 
     def package(self):
-        cmake = self._configure_cmake()
+        cmake = CMake(self)
         cmake.install()
 
     def package_info(self):
+        #self.cpp_info.libdirs = ["lib"]
         self.cpp_info.libs = ["DSPFilters"]
+        #self.cpp_info.includedirs = ["include"]
+        self.output.info(f"libdirs {self.cpp_info.libdirs}")
+        self.output.info(f"libs: {self.cpp_info.libs}")
+        self.output.info(f"includedirs: {self.cpp_info.includedirs}")
