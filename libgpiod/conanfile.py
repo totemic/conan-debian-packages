9942116ef2c990611d90d6b8428a0b70feddd21c
Jens <jens@totemic.com>
2023-07-14 16:37:47 -0700

    Upgraded libgpiod to conan 2

--- libgpiod/conanfile.py
+++ libgpiod/conanfile.py
@@ -1,9 +1,13 @@
-import os
-from os.path import join
-from conans import ConanFile, AutoToolsBuildEnvironment, tools
+from conan import ConanFile
+from conan.errors import ConanInvalidConfiguration
+from conan.tools.build import cross_building
+from conan.tools.env import VirtualBuildEnv, VirtualRunEnv
+from conan.tools.files import get, copy
+from conan.tools.gnu import Autotools, AutotoolsDeps, AutotoolsToolchain, PkgConfigDeps
+from conan.tools.layout import basic_layout
+from pathlib import Path
 
-
-SOURCE_SUBFOLDER = "sources"
+required_conan_version = ">=1.53.0"
 
 # TODO: switch to https://github.com/conan-io/conan-center-index/blob/master/recipes/libgpiod once it supports v1.2.1
 
@@ -19,39 +23,64 @@ class LibgpiodConan(ConanFile):
     options = {"shared": [True, False], "fPIC": [True, False]}
     default_options = {"shared": False, "fPIC": True}
 
-    def source(self):
-        tools.get(**self.conan_data["sources"][self.version], destination=SOURCE_SUBFOLDER, strip_root=True)
-
     def build_requirements(self):
         if self.settings.os == "Linux":
-            installer = tools.SystemPackageTool()
-            installer.install("autoconf-archive")
+            pass
+            # installer = tools.SystemPackageTool()
+            # installer.install("autoconf-archive")
+            # self.build_requires("libtool/2.4.6")
+            # self.build_requires("pkgconf/1.7.4")
+            # self.build_requires("autoconf-archive/2021.02.19")
+            # self.build_requires("linux-headers-generic/5.13.9")
 
-    def _configure_autotools(self, folder):
-        cfg_args = ["--enable-bindings-cxx", "--enable-tools"]
-        if self.options.shared:
-            cfg_args += ["--enable-shared", "--disable-static"]
-        else:
-            cfg_args += ["--disable-shared", "--enable-static"]
+    def layout(self):
+        basic_layout(self, src_folder="src")
 
-        autotools = AutoToolsBuildEnvironment(self)
+    def source(self):
+        get(self, **self.conan_data["sources"][self.version], strip_root=True)
 
-        # fixes error "undefined reference to `rpl_malloc'"
-        env_vars = autotools.vars
-        env_vars["ac_cv_func_malloc_0_nonnull"] = "yes"
+    def generate(self):
+        if self.settings.os != "Linux":
+            return
 
-        autotools.configure(configure_dir=folder, args=cfg_args, vars=env_vars)
-        return autotools
+        # env = VirtualBuildEnv(self)
+        # env.generate()
+        # if not cross_building(self):
+        #     env = VirtualRunEnv(self)
+        #     env.generate(scope="build")
+
+        tc = AutotoolsToolchain(self, prefix="")
+
+        # these setting not needed, are added automatically by AutotoolsToolchain 
+        # if self.options.shared:
+        #     cfgArgs += ["--enable-shared", "--disable-static"]
+        # else:
+        #     cfgArgs += ["--disable-shared", "--enable-static"]
+        #tc.fpic = self.options.get_safe("fPIC", True)
+
+        tc.configure_args.extend([
+            "--enable-bindings-cxx", 
+            "--enable-tools"
+        ])
+        if cross_building(self):
+            # fixes error "undefined reference to `rpl_malloc'"
+            tc.configure_args.append("ac_cv_func_malloc_0_nonnull=yes")
+            tc.configure_args.append("ac_cv_func_realloc_0_nonnull=yes")
+        tc.generate()
+
+        # env = tc.environment()
+        # # fixes error "undefined reference to `rpl_malloc'"
+        # env.define("ac_cv_func_malloc_0_nonnull", "yes")
+        # tc.generate(env)
+
+        AutotoolsDeps(self).generate()
+        # PkgConfigDeps(self).generate()
 
     def build(self):
-        source_folder = join(self.source_folder, SOURCE_SUBFOLDER)
-
         if self.settings.os == "Linux":
-            # autoreconf changes the content of the source folder, but for automatic build this
-            # is fine as the source folder is copied into the build folder first
-            self.run("autoreconf --force --install --verbose", cwd=source_folder)
-
-            autotools = self._configure_autotools(source_folder)
+            autotools = Autotools(self)
+            autotools.autoreconf()
+            autotools.configure()
             autotools.make()
         else:
             # We allow using it on all platforms, but for anything except Linux nothing is produced
@@ -59,16 +88,13 @@ class LibgpiodConan(ConanFile):
             self.output.info("Nothing to be done for this OS")
 
     def package(self):
-        source_folder = join(self.source_folder, SOURCE_SUBFOLDER)
-
         if self.settings.os == "Linux":
-            # Reconfigure to update the package folder
-            autotools = self._configure_autotools(source_folder)
+            autotools = Autotools(self)
             autotools.install()
         else:
             # On non-linux platforms, expose the header files to help cross-development
-            self.copy(pattern="*.h", dst="include", src=join(source_folder, "include"), symlinks=True)
-            self.copy(pattern="*.hpp", dst="include", src=join(source_folder, "bindings/cxx"), symlinks=True)
+            copy(self, pattern="*.h", src=Path(self.source_folder)/"include", dst=Path(self.package_folder)/"include")
+            copy(self, pattern="*.hpp", src=Path(self.source_folder)/"bindings"/"cxx", dst=Path(self.package_folder)/"include")
 
     def package_info(self):
         if self.settings.os == "Linux":
