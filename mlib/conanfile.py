ad53cc40c96e9d7c7688b029ba86d2c2926df237
Jens <jens@totemic.com>
2023-07-13 17:24:25 -0700

    Use conandata and github release instead of commit

--- mlib/conanfile.py
+++ mlib/conanfile.py
@@ -6,24 +6,17 @@ from conans import ConanFile, AutoToolsBuildEnvironment, tools
 class MlibConan(ConanFile):
     name = "mlib"
     version = "0.7.0"
-    source_subfolder = "mlib"
-    scm = {
-        "type": "git",
-        "subfolder": source_subfolder,
-        "url": "https://github.com/P-p-H-d/mlib.git",
-        "revision": "V" + version
-    }
-
     homepage = "https://github.com/P-p-H-d/mlib"
     description = "Library for using generic and type safe container in pure C language (C99 or C11) for a wide collection of container (comparable to the C++ STL)."
-    url = "https://github.com/jens-totemic/conan-mlib"
+    url = "https://github.com/totemic/conan-package-recipes/tree/main/mlib"
     exports = "LICENSE"
 
+    def source(self):
+        tools.get(**self.conan_data["sources"][self.version], destination=".", strip_root=True)
+
     def package(self):
-        with tools.chdir(self.source_subfolder):
-            #self.copy("LICENSE")
-            self.copy("*.h", excludes=["mlib/bench/*", "mlib/tests/*"])
+        self.copy("*.h", excludes=["bench/*", "tests/*"], dst="mlib")
             
     def package_info(self):
-            self.cpp_info.include_paths.append(self.package_folder)
-            self.output.info("include_paths: %s" % self.cpp_info.include_paths)
+        self.cpp_info.include_paths.append(self.package_folder)
+        self.output.info("include_paths: %s" % self.cpp_info.include_paths)
